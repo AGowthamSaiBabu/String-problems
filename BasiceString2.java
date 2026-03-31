944ed42ce5cf3705f2204c641f03db77aa60389e
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-03-31 17:34:38 +0530

    BasiceString2

---
+++ BasiceString2.java
@@ -0,0 +1,46 @@
+// 1. Print a String
+
+// public class BasiceString2 {
+//     public static void main(String[] args) {
+//         String s ="Hello java";
+
+//         System.out.println(s);
+//     }
+// }
+
+// 2. Find Length of a String
+
+// public class BasiceString2 {
+//     public static void main(String[] args) {
+//         String s ="Hello";
+
+//         System.out.println(s.length());
+//     }
+// }
+
+// 3. Concatenate Two Strings
+
+// public class BasiceString2 {
+//     public static void main(String[] args) {
+//         String s ="Hello";
+//         String s1 ="java";
+
+//         String result =s+s1;
+
+//         System.out.println(result);
+//     }
+// }
+
+// 4. Compare Two Strings
+public class BasiceString2 {
+    public static void main(String[] args) {
+        String s1 ="Java";
+        String s2 ="Java";
+
+       if(s1==s2){
+        System.out.println("String are Equal");
+       }else{
+        System.out.println("not equal");
+       }
+    }
+}
