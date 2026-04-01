160e5108751f80da4ceddc11888502abcd218a36
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-01 21:23:39 +0530

    BasiceString3

---
+++ BasiceString3.java
@@ -0,0 +1,32 @@
+// 5. Convert String to Uppercase
+
+// public class BasiceString3 {
+//    public static void main(String[] args) {
+//      String s = "java";
+
+//      System.out.println(s.toUpperCase());
+//    } 
+// }
+
+// 6 Convert String to Lowercase
+
+// public class BasiceString3 {
+//    public static void main(String[] args) {
+//      String s = "JAVA";
+
+//      System.out.println(s.toLowerCase());
+//    } 
+// }
+
+// 7. Check If String Contains a Substring
+
+public class BasiceString3 {
+   public static void main(String[] args) {
+     String s = "Welcome to Java";
+     String s1 = "Java";
+
+     boolean result =s.contains(s1);
+
+     System.out.println(result);
+   } 
+}
