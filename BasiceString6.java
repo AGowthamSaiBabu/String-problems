7abc449c376e3c2d8ac70782d2d9dbb9f61498e5
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-11 17:52:34 +0530

    BasiceString6

---
+++ BasiceString6.java
@@ -0,0 +1,29 @@
+// 13. Check if String Ends With a Word
+
+// public class BasiceString6 {
+//     public static void main(String[] args) {
+//         String str ="Hello Java";
+//         String word ="Java";
+
+//         boolean result = str.endsWith(word);
+
+//         System.out.println(result);
+//     }
+// }
+
+// 14. Count Occurrence of a Character
+
+public class BasiceString6 {
+    public static void main(String[] args) {
+        String str ="banana";
+        char ch ='a';
+        int count =0;
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==ch){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
