4b18b81c3ae12239bea1b3388c161a9e55e8576b
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-13 08:43:03 +0530

    BasiceString6

--- BasiceString6.java
+++ BasiceString6.java
@@ -13,17 +13,26 @@
 
 // 14. Count Occurrence of a Character
 
+// public class BasiceString6 {
+//     public static void main(String[] args) {
+//         String str ="banana";
+//         char ch ='a';
+//         int count =0;
+
+//         for(int i=0;i<str.length();i++){
+//             if(str.charAt(i)==ch){
+//                 count++;
+//             }
+//         }
+//         System.out.println(count);
+//     }
+// }
+
+// 15. Remove All Whitespaces from a String
 public class BasiceString6 {
     public static void main(String[] args) {
-        String str ="banana";
-        char ch ='a';
-        int count =0;
-
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)==ch){
-                count++;
-            }
-        }
-        System.out.println(count);
+    String str ="Java is cool";
+    String result = str.replaceAll("\\s","");
+    System.out.println(result);
     }
 }
