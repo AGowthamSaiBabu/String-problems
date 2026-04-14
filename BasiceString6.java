20551b8966eb8d12e0002bdef4558c4397776d6d
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-14 17:00:53 +0530

    BasiceString6

--- BasiceString6.java
+++ BasiceString6.java
@@ -29,10 +29,29 @@
 // }
 
 // 15. Remove All Whitespaces from a String
+// public class BasiceString6 {
+//     public static void main(String[] args) {
+//     String str ="Java is cool";
+//     String result = str.replaceAll("\\s","");
+//     System.out.println(result);
+//     }
+// }
+
+// 16 Right rotate the array by one
 public class BasiceString6 {
     public static void main(String[] args) {
-    String str ="Java is cool";
-    String result = str.replaceAll("\\s","");
-    System.out.println(result);
+        int [] arr ={1,2,3,4};
+        int n =arr.length;
+
+        int temp =arr[n-1];
+
+        for(int i = n-1;i>0;i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0]=temp;
+
+        for(int i =0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
