ee9e62fa362c78a4a4ae0af8c61c817485f19453
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-18 16:34:15 +0530

    BasiceString7

--- BasiceString7.java
+++ BasiceString7.java
@@ -1,16 +1,35 @@
 // 20. Remove Duplicate Characters
 
+// public class BasiceString7 {
+//     public static void main(String[] args) {
+//         String str = "programmer";
+//         String result = "";
+
+//         for (int i = 0; i < str.length(); i++) {
+//             char ch = str.charAt(i);
+//             if (result.indexOf(ch) == -1) {
+//                 result += ch;
+//             }
+//         }
+//         System.out.println(result);
+//     }
+// }
+
+// 21. Find Duplicate Characters
+
 public class BasiceString7 {
     public static void main(String[] args) {
-        String str = "programmer";
-        String result = "";
+        String str ="banana";
 
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (result.indexOf(ch) == -1) {
-                result += ch;
+        int count [] =new int [256];
+        for(int i=0;i<str.length();i++){
+            count[str.charAt(i)]++;
+            
+        }
+        for(int i=0;i<256;i++){
+            if(count[i]>1){
+                System.out.println((char)i+" ");
             }
         }
-        System.out.println(result);
     }
 }
