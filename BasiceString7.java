bdb08680191e982ad5718d9ccd6d56b7466c93cc
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-19 17:30:27 +0530

    BasiceString7

--- BasiceString7.java
+++ BasiceString7.java
@@ -17,18 +17,32 @@
 
 // 21. Find Duplicate Characters
 
+// public class BasiceString7 {
+//     public static void main(String[] args) {
+//         String str ="banana";
+
+//         int count [] =new int [256];
+//         for(int i=0;i<str.length();i++){
+//             count[str.charAt(i)]++;
+            
+//         }
+//         for(int i=0;i<256;i++){
+//             if(count[i]>1){
+//                 System.out.println((char)i+" ");
+//             }
+//         }
+//     }
+// }
+
+// 22. Print All Substrings of a String
+
 public class BasiceString7 {
     public static void main(String[] args) {
-        String str ="banana";
+        String str ="abc";
 
-        int count [] =new int [256];
         for(int i=0;i<str.length();i++){
-            count[str.charAt(i)]++;
-            
-        }
-        for(int i=0;i<256;i++){
-            if(count[i]>1){
-                System.out.println((char)i+" ");
+            for(int j=i+1;j<=str.length();j++){
+                System.out.println(str.substring(i, j));
             }
         }
     }
