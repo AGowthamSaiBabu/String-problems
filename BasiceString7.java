70a9b7f471574aad9b1be8a17ca58cfe7c85689e
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-20 16:25:39 +0530

    BasiceString7

--- BasiceString7.java
+++ BasiceString7.java
@@ -36,14 +36,36 @@
 
 // 22. Print All Substrings of a String
 
+// public class BasiceString7 {
+//     public static void main(String[] args) {
+//         String str ="abc";
+
+//         for(int i=0;i<str.length();i++){
+//             for(int j=i+1;j<=str.length();j++){
+//                 System.out.println(str.substring(i, j));
+//             }
+//         }
+//     }
+// }
+
+// 24. Toggle Case of Each Character
+
 public class BasiceString7 {
     public static void main(String[] args) {
-        String str ="abc";
+        String str="JaVa";
+        String result ="";
 
         for(int i=0;i<str.length();i++){
-            for(int j=i+1;j<=str.length();j++){
-                System.out.println(str.substring(i, j));
+            char ch =str.charAt(i);
+
+                if(Character.isUpperCase(ch)){
+                    result += Character.toLowerCase(ch);
+                }else if(Character.isLowerCase(ch)){
+                    result +=Character.toUpperCase(ch);
+                }else{
+                    result +=ch;
             }
         }
+        System.out.println(result);
     }
 }
