4e84e6f4b9a15ebb64220140f819c4d74e7f0b38
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-23 12:04:58 +0530

    BasiceString8

--- BasiceString8.java
+++ BasiceString8.java
@@ -1,15 +1,35 @@
 // 25. Remove a Specific Character
 
+// public class BasiceString8 {
+//     public static void main(String[] args) {
+//         String str ="apple";
+//         char ch ='p';
+
+//         String result ="";
+
+//         for(int i=0;i<str.length();i++){
+//             if(str.charAt(i) !=ch){
+//                 result +=str.charAt(i);
+//             }
+//         }
+//         System.out.println(result);
+//     }
+// }
+
+// 26. Check if a String is Made of Only Alphabets
+
 public class BasiceString8 {
     public static void main(String[] args) {
-        String str ="apple";
-        char ch ='p';
+        String str ="Java123";
 
-        String result ="";
+        boolean result =true;
 
         for(int i=0;i<str.length();i++){
-            if(str.charAt(i) !=ch){
-                result +=str.charAt(i);
+            char ch =str.charAt(i);
+
+            if(!Character.isLetter(ch)){
+                result =false;
+                break;
             }
         }
         System.out.println(result);
