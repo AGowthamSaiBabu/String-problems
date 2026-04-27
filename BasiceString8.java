25a39a9393cf8676bc2cd3a5b1cbe7e93342742a
AGowthamSaiBabu <allamgowthamsaibabu@gmail.com>
2026-04-27 16:56:16 +0530

    BasiceString8

--- BasiceString8.java
+++ BasiceString8.java
@@ -38,23 +38,44 @@
 
 // 27. Count Capital and Small Letters Separately
 
+// public class BasiceString8 {
+//     public static void main(String[] args) {
+//         String str ="JaVA123";
+//         int upper =0;
+//         int lower =0;
+
+//         for(int i=0;i<str.length();i++){
+//             char ch =str.charAt(i);
+
+//             if(Character.isUpperCase(ch)){
+//                 upper++;
+//             }else if(Character.isLowerCase(ch)){
+//                 lower++;
+//             }
+//         }
+//         System.out.println("Uppercase"+upper);
+//         System.out.println("Lowercase"+lower);
+        
+//     }
+// }
+
+// 28. Find First Non-Repeated Character
+
 public class BasiceString8 {
     public static void main(String[] args) {
-        String str ="JaVA123";
-        int upper =0;
-        int lower =0;
+        String str ="swiss";
+
+        int [] count =new int[256];
 
         for(int i=0;i<str.length();i++){
-            char ch =str.charAt(i);
-
-            if(Character.isUpperCase(ch)){
-                upper++;
-            }else if(Character.isLowerCase(ch)){
-                lower++;
+            count[str.charAt(i)]++;
+        }
+        for(int i=0;i<str.length();i++){
+            if(count[str.charAt(i)]==1){
+                System.out.println("First non-repeated character:"+str.charAt(i));
+                return;
             }
         }
-        System.out.println("Uppercase"+upper);
-        System.out.println("Lowercase"+lower);
-        
+        System.out.println("No non-repeated character found");
     }
 }
